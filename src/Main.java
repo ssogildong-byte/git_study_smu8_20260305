66721ca70ab3e0606c5b78ed0caac0c694bd86ae
ssogildong-byte <ssogildong@gmail.com>
2026-03-05 14:41:37 +0900

    이슈해결

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,8 @@ public class Main {
         }
 
     public static void main(String[] args) {
+        System.out.println("test");
+        //추가
 
         //TIP 캐럿을 강조 표시된 텍스트에 놓고 <shortcut actionId="ShowIntentionActions"/>을(를) 누르면
         // IntelliJ IDEA이(가) 수정을 제안하는 것을 확인할 수 있습니다.

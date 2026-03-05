c259c3f54e4739c482c8a10c39e066e35dc92b4a
ssogildong-byte <ssogildong@gmail.com>
2026-03-05 15:18:27 +0900

    성적 스위치 추가

--- src/Main.java
+++ src/Main.java
@@ -17,6 +17,14 @@ public class Main {
         System.out.println("test");
         System.out.println("test");
         //추가
+        int score=77;
+        String grade=switch(score/10){
+
+            case 7->"c";
+
+            default -> throw new IllegalStateException("Unexpected value: " + score);
+        };
+
 
         //TIP 캐럿을 강조 표시된 텍스트에 놓고 <shortcut actionId="ShowIntentionActions"/>을(를) 누르면
         // IntelliJ IDEA이(가) 수정을 제안하는 것을 확인할 수 있습니다.

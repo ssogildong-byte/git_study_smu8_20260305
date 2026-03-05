e39e3a859528b6a12a80adc01b1f6e4de865ee35
ssogildong-byte <ssogildong@gmail.com>
2026-03-05 14:36:07 +0900

    sum 수정완료

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
     public void sum(int a, int b){
         System.out.printf("a+b=%d", a+b);
+        System.out.println("hotfix 수정중1");
+        System.out.println("hotfix 수정중2");
+        System.out.println("hotfix 수정중3");
     }
 
     public void b(){
@@ -20,6 +23,7 @@ public class Main {
             //TIP <shortcut actionId="Debug"/>을(를) 눌러 코드 디버그를 시작하세요. 1개의 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 중단점을 설정해 드렸습니다
             // 언제든 <shortcut actionId="ToggleLineBreakpoint"/>을(를) 눌러 중단점을 더 추가할 수 있습니다.
             System.out.println("i = " + i);
+
         }
     }
 }
